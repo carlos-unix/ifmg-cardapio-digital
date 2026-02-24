f857c9818523cf37f03a507906e29dad106d3d8d
carlos-unix <ceduardolopes999@gmail.com>
2026-02-23 22:18:01 -0300

    Only one function new file

--- bot/src/main/java/ifmgbot/Connect.java
+++ bot/src/main/java/ifmgbot/Connect.java
@@ -5,61 +5,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 // function to create connection
-/* NOTA: nem todos os campus estão disponíveis para consultar o cardápio digital, por isso há apenas alguns links de conexão. */
-
 public class Connect {
     public static String UserAgent = "Mozilla/5.0";
     public static Document doc = null;
 
-    public static Document createConnectionSJE() {
+    public static Document createConnection(String link) {
         try {
             doc = Jsoup
-                    .connect("https://cardapio.sje.ifmg.edu.br/cardapio.php")
-                    .userAgent(UserAgent)
-                    .get();
-
-        } catch (IOException e) {
-            System.out.println("Erro ao acessar o site:");
-            e.printStackTrace();
-        }
-
-        return doc;
-    }
-
-    public static Document createConnectionGVA() {
-        try {
-            doc = Jsoup
-                    .connect("https://www.ifmg.edu.br/governadorvaladares/central-de-servicos/cardapio-restaurante")
-                    .userAgent(UserAgent)
-                    .get();
-
-        } catch (IOException e) {
-            System.out.println("Erro ao acessar o site:");
-            e.printStackTrace();
-        }
-
-        return doc;
-    }
-
-    public static Document createConnectionOUP() {
-        try {
-            doc = Jsoup
-                    .connect("https://ouropreto.ifmg.edu.br/ouropreto/central-de-servicos/cardapio-restaurante")
-                    .userAgent(UserAgent)
-                    .get();
-
-        } catch (IOException e) {
-            System.out.println("Erro ao acessar o site:");
-            e.printStackTrace();
-        }
-
-        return doc;
-    }
-
-    public static Document createConnectionFOR() {
-        try {
-            doc = Jsoup
-                    .connect("https://plone.ifmg.edu.br/formiga/central-de-servicos/cardapio-restaurante")
+                    .connect(link)
                     .userAgent(UserAgent)
                     .get();
 
